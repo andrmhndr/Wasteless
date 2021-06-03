39e9042da67982c1ecd5b6962dc1756ab6b9da8c
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2021-06-03 16:26:37 +0700

    new

--- app/src/main/java/com/example/wasteless/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/wasteless/viewmodel/UserViewModel.kt
@@ -8,6 +8,7 @@ import com.example.wasteless.model.UserModel
 import com.example.wasteless.utils.Helper
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.tasks.await
 
 class UserViewModel: ViewModel() {
